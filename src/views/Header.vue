3fe5e9b3158c10588a5c5020ad31994f5d8fac60
Okegbemi Olatunde <okegbemiolatunde@gmail.com>
2021-02-10 23:14:48 +0100

    First Draft

---
+++ src/views/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container mx-auto">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/logo.png" alt="Ennovate Lab" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-link active" aria-current="page" href="#">Why attend?</a>
+                    <a class="nav-link" href="#">Speaker</a>
+                    <a class="nav-link" href="#">Schedule</a>
+                    <a class="nav-link" href="#">Join</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+  
+    .navbar {
+
+        // padding: 1.2rem 10rem;
+        padding: 1.2rem 0rem;
+        background: #000;
+        
+        &-brand {
+            img {
+                width: 10rem;
+            }
+        }
+
+        &-nav {
+
+            .nav-link{
+                color: #fff;
+
+                &:hover {
+                    color: #FFc83d;
+                }
+            }
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,468 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <section class="hero">
+            <section class="container">
+                <div class="row align-items-end">
+                    <div class="col-md-6 col-lg-7 hero__info">
+                        <h1 class="hero__info--main">Product Framework for Product Developers, Designers, and Managers.</h1>
+                        <p class="hero__info--subtitle">Join the conversation on how to use Product Field to map out your world-changing ideas and build better products.</p>
+                        
+                        <p class="hero__info--date">Date: 22nd February, 2021</p>
+                        <p class="hero__info--time">Time: 4pm</p>
+
+                        <a href="#" target="_blank" rel="noreferrer noopener" class="btn hero__info--btn-join">
+                            Join Now
+                        </a>
+
+                        <a href="#" target="_blank" rel="noreferrer noopener" class="btn hero__info--btn-watch">
+                            <span class="fas fa-play-circle"></span>
+                            Watch the Video
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-5 hero__img">
+                        <!-- <img src="../assets/metrics.png" alt="Virtually helps you do less to achieve more" class="img-fluid"> -->
+                        <img src="../assets/metrics.svg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </section>
+        </section>
+
+        <section class="attend">
+            <section class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-lg-6 attend__img">
+                        <img src="../assets/friends.svg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 col-lg-6 attend__info">
+                        <h2 class="attend__info--title">Why Attend?</h2>
+
+                        <p class="attend__info--subtitle">We all know how daunting creating a product that:</p>
+
+                        <ul class="attend__info--list">
+                            <li>meet all the needs of your users,</li>
+                            <li>aligns with your vision,</li>
+                            <li>as well as the perspective of the stakeholders.</li>
+                        </ul>
+                        <p class="attend__info--subtitle">could be.</p>
+
+                        <p class="attend__info--text-1">Solving for this has been the quest of many brilliant minds over the years. Some years back, three intrepid men, <b> KP Frahm, Michael Schieben </b> and <b> Wolfgang Wopperer-Beholz, </b> decided to take on the challenge and Product Field was the outcome.</p>
+                        <p class="attend__info--text-2">The Product Field framework was created to help you align your goals and perspectives with that of your stakeholders, navigate through the product complexities, and help you build a better product for your users with confidence.</p>
+
+                        <a href="#" target="_blank" rel="noreferrer noopener" class="btn attend__info--btn">
+                            Join Now
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+        <section class="speakers">
+            <section class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-lg-6 speakers__info">
+                        <div class="speakers__info--title">
+                            <div class="speakers__info--title-img">
+                                <img src="../assets/michael.png" class="" alt="">
+                            </div>
+                            <div class="speakers__info--title-name">
+                                <h3 class="role">Speaker</h3>
+                                <h4 class="name">Michael Schieben</h4>
+                                <p class="position">Co-founder/Managing Director/ Product Lead at <a href="#">Field.</a> </p>
+                            </div>
+                        </div>
+                        <div class="speakers__info--detail">
+                            <p>Michael Schieben is a co-founder at <a href="#" class="link">Field.so </a> , a startup working on software for innovative product management, and the company responsible for creating the Product Field Framework. </p>
+                            <p>Michael's craft is programming, his mindset is Design. He helps others create and collaborate on sustainable product and service innovation. He facilitates system we-thinking, co-design, and product development processes. </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-6 speakers__info">
+                        <div class="speakers__info--title">
+                            <div class="speakers__info--title-img">
+                                <img src="../assets/jd.png" class="" alt="">
+                            </div>
+                            <div class="speakers__info--title-name">
+                                <h3 class="role">Host</h3>
+                                <h4 class="name">Jesudamilare “JD” Adesegun-David</h4>
+                                <p class="position">Co-founder <a href="https://www.ennovatelab.com" target="_blank">Ennovate Lab.</a> </p>
+                            </div>
+                        </div>
+                        <div class="speakers__info--detail">
+                            <p>Jesudamilare "JD" Adesegun-David is a Thought Alchemist and a Community Transformation Strategist. He is a promoter of a Communities-as-Innovation-Hubs approach to positioning Africa for relevance in a global digital and knowledge economy.</p>
+                            <p><a href="https://www.ennovatelab.com" target="_blank"> Ennovate Lab </a> is a community, an innovation Hub and a Startup Foundry committed to creating borderless innovative solutions for communities of all kinds.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+        <section class="schedule">
+            <section class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-lg-7 schedule__info">
+                        <h2 class="schedule__info--title">Schedule</h2>
+
+                        <div class="row schedule__info-bl">
+                            <div class="col-md-5 schedule__info--first">
+                                <h6>4:00pm - 4:05pm</h6>
+                                <h6>Welcome: <span>JD</span> </h6>
+                            </div>
+                            <div class="col-md-7 schedule__info--second">
+                                <h6>4:05pm - 4:15pm</h6>
+                                <h6>Introduction: <span>Michael</span> </h6>
+                                <p>A brief introduction to the visual form of the Product Field.</p>
+                            </div>
+                        </div>
+
+                        <div class="row schedule__info-bl">
+                            <div class="col-md-5 schedule__info--first">
+                                <h6>4:20pm - 4:45pm</h6>
+                                <h6>Show not Tell: <span>JD &amp; Michael</span> </h6>
+                                <p>Live Mapping of Trifold</p>
+                            </div>
+                            <div class="col-md-7 schedule__info--second">
+                                <h6>4:45pm - 5:00pm</h6>
+                                <h6>Innovation Methodologies: <span>Michael</span> </h6>
+                                <p>How the visual form of the Product Field helps to understand, organize and use other innovation methodologies.</p>
+                            </div>
+                        </div>
+
+                        <div class="row schedule__info-bl">
+                            <div class="col-md-5 schedule__info--first">
+                                <h6>5:00pm - 5:20pm</h6>
+                                <h6>Q&amp;A: <span>JD &amp; Michael</span> </h6>
+                                <p>Questions from the audience</p>
+                            </div>
+                            <div class="col-md-7 schedule__info--second">
+                                <h6>5:20pm - 5:30pm</h6>
+                                <h6>Closing: <span>JD</span> </h6>
+                                <p>Additional Resources, Closing Thoughts</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-6 col-lg-5 schedule__img">
+                        <img src="../assets/schedule.svg" alt="" class="img-fluid">
+                    </div>
+
+                </div>
+            </section>
+        </section>
+
+        <section class="questions">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Questions?</h2>
+                        <p>Shoot an email to <b> hello@ennovatelab.com </b> or <b> hello@field.so </b> </p>
+
+                        <a href="#" target="_blank" rel="noreferrer noopener" class="btn questions__btn-join">
+                            Join Now
+                        </a>
+                        <a href="#" target="_blank" rel="noreferrer noopener" class="btn questions__btn-watch">
+                            <span class="fas fa-play-circle"></span>
+                            Watch the Video
+                        </a>
+                    </div>
+                </div>
+            </div> 
+        </section>
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style lang="scss">
+    .home {
+
+        .hero {
+            background: url('../assets/bg@hero.jpg');
+            height: 90vh;
+
+            &__info {
+                &--main {
+                    color: #FFC83D;
+                    font-size: 3rem;
+                    font-weight: bold;
+                }
+
+                &--subtitle {
+                    color: #fff;
+                    font-size: 1.5rem;
+                    margin-bottom: 5rem;
+                }
+
+                &--date {
+                    border-left: 3px solid #FFC83D;
+                    color: #fff;
+                    font-size: 1.2rem;
+                    padding-left: .5rem;
+                    // margin-bottom: 3rem;
+                }
+
+                &--time {
+                    border-left: 3px solid #5f17f9;
+                    color: #fff;
+                    font-size: 1.2rem;
+                    padding-left: .5rem;
+                    margin-bottom: 3rem;
+                }
+
+                &--btn {
+                    
+                    &-join{
+                        
+                        border-radius: 100px;
+                        border: none;
+                        padding: 1rem 3rem;
+                        font-size: 1.2rem;
+                        font-weight: bold;
+                        color: #fff;
+                        background: linear-gradient(90.43deg, #FFc83d 28.99%, #5F17F9 99.74%);
+                        margin-right: .5rem;
+                    }
+
+                    &-watch{
+                        border-radius: 100px;
+                        background: transparent;
+                        border: none;
+                        padding: 1rem 1rem;
+                        font-size: 1rem;
+                        color: #fff;
+                        font-size: 1rem;
+                    }
+                }
+            }
+        }
+
+        .attend {
+            background: url('../assets/bg@why.svg') #FFc83d;
+
+            padding: 7rem .5rem;
+
+            &__img {
+                text-align: left;
+            }
+
+            &__info {
+                &--title{
+                    font-weight: bold;
+                    color: #000;
+                    font-size: 1.7rem;
+                    margin-bottom: 3rem;
+                    position: relative;
+                    padding-bottom: .5rem;
+
+                    &:before {
+                        content: "";
+                        position: absolute;
+                        width: 50px;
+                        height: 1px;
+                        bottom: 0;
+                        left: 1%;
+                        border-bottom: 5px solid #000;
+                    }
+                }
+
+                &--subtitle{
+                    font-size: 1.1rem;
+                }
+
+                &--list{
+                    padding-left: 0;
+                    li {
+                        list-style-position: inside; 
+                        list-style-type: square;   
+                        font-size: 1.1rem;
+                    }
+                }
+
+                &--text-1{
+                    font-size: 1.1rem;
+                }
+                &--text-2{
+                    font-size: 1.1rem;
+                    margin-bottom: 2rem;
+                }
+                &--btn{
+                    border-radius: 100px;
+                    background: black;
+                    border: none;
+                    padding: .75rem 3rem;
+                    font-size: 1rem;
+                    color: #fff;
+                    font-size: 1rem;
+                }
+            }
+        }
+
+        .speakers {
+            background: url('../assets/speakers.jpg');
+            padding: 8rem .5rem;
+
+            &__info {
+                &--title {
+                    
+                    display: flex;
+                    justify-items: space-between;
+                    align-items: flex-end;
+                    margin-bottom: 2rem;
+
+                    &-img {
+                        margin-right: 2rem;
+                        img {
+                            width: 12rem;
+                        }
+                    }
+
+                    &-name {
+                        .role {
+                            color: #FFc83d;
+                            position: relative;
+                            padding-bottom: 1rem;
+                            margin-bottom: 2rem;
+
+                            &:before {
+                                content: "";
+                                position: absolute;
+                                width: 50px;
+                                height: 1px;
+                                bottom: 0;
+                                left: 1%;
+                                border-bottom: 3px solid #FFc83d;
+                            }
+                        }
+                        .position{
+                            color: lightgray;
+
+                            a{
+                                color: #FFc83d;
+                                font-weight: bold;
+                            }
+                        }
+
+                        .name {
+                            color: #fff;
+                            font-weight: bold;
+                        }
+                    }
+                }
+
+                &--detail {
+                    p{
+                        color: #fff;
+
+                        a{
+                            color: #FFc83d;
+                            font-weight: bold;
+                        }
+                    }
+                }
+            }
+        }
+
+        .schedule {
+            padding: 7rem .5rem;
+            background: url('../assets/bg@schedule.svg') #FFc83d;
+
+            &__info {
+                &--title{
+                    font-weight: bold;
+                    color: #000;
+                    font-size: 1.7rem;
+                    margin-bottom: 3rem;
+                    position: relative;
+                    padding-bottom: .5rem;
+
+                    &:before {
+                        content: "";
+                        position: absolute;
+                        width: 50px;
+                        height: 1px;
+                        bottom: 0;
+                        left: 0%;
+                        border-bottom: 5px solid #000;
+                    }
+                }
+
+                &-bl {
+                    margin-left: 0;
+                    margin-bottom: 1rem;
+                }
+                
+                &--first {
+                    background: #fff;
+                    padding: 1.5rem;
+                    border-top-left-radius: .75rem;
+                    border-bottom-left-radius: .75rem;
+
+                    span {
+                        color: #FFc83d;
+                    }
+                }
+                
+                &--second {
+                    padding: 1.5rem;
+                    background: #ffeec1;
+                    border-top-right-radius: .75rem;
+                    border-bottom-right-radius: .75rem;
+
+                    span {
+                        color: #FFc83d;
+                    }
+                }
+     
+            }
+        }
+
+        .questions {
+            text-align: center;
+            background: #000;
+            padding: 7rem 1rem; 
+            color: #fff;
+
+            h2 {
+                color: #FFc83d;
+                margin-bottom: 3rem;
+            }
+
+            p {
+                margin-bottom: 2rem;
+
+                b {
+                    color:#FFc83d;
+                }
+            }
+
+            &__btn {
+                
+                &-join{
+                    
+                    border-radius: 100px;
+                    border: none;
+                    padding: .75rem 3rem;
+                    font-size: 1rem;
+                    font-weight: bold;
+                    color: #fff;
+                    background: linear-gradient(90.43deg, #FFc83d 28.99%, #5F17F9 99.74%);
+                    margin-right: .5rem;
+                }
+
+                &-watch{
+                    border-radius: 100px;
+                    background: transparent;
+                    border: none;
+                    padding: .75rem 1rem;
+                    font-size: 1rem;
+                    color: #fff;
+                    font-size: 1rem;
+                }
+            }
+        }
+    }
+</style>
